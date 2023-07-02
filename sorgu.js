ef852428765bccea00b176b385de1fd437ad30bd
Can <63351166+fastuptime@users.noreply.github.com>
2023-07-02 21:14:48 +0300

    Update sorgu.js

--- sorgu.js
+++ sorgu.js
@@ -10,7 +10,7 @@ async function sorgu(id) {
             'accept': '*/*', 
             'accept-language': 'tr', 
             'cookie': 'ig_did=AADD2AB0-0929-48E6-A6ED-DD616D7C8690; datr=VnShZEIQ64YS10Xm46D35i7C; csrftoken=efo7t57nh46Hmxokragaj5TAjTnsNLUL; mid=ZKF0VwALAAFxGt7LB7lOdyV8j_e3; ig_nrcb=1; csrftoken=VQEIFgxFg27BzkfGlDjCWYzy4qbE6Beu; ds_user_id=51870645423; ig_did=F76D79C3-D228-47D3-82BA-250C84EC0543; ig_nrcb=1; mid=ZKCCiwAEAAFq3S8OTFrpmrjoGvZh; rur="CLN\\05451870645423\\0541719837985:01f744e5528d3d8bc0124dbb7418ef6455afef0da218f001561cc84b315988f4c3ea6a90"', 
-            'referer': 'https://www.instagram.com/explore/locations/72813118/dalaman/', 
+            'referer': 'https://www.instagram.com/explore/locations/72813118/ankara/', 
             'sec-ch-prefers-color-scheme': 'dark', 
             'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"', 
             'sec-ch-ua-full-version-list': '"Not.A/Brand";v="8.0.0.0", "Chromium";v="114.0.5735.199", "Google Chrome";v="114.0.5735.199"', 
